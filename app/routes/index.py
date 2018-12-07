bc094e47ebeeec32c49d6b24f2c584855da922e3
Toufiq <burningraven06@gmail.com>
2018-12-07 18:59:01 +0600

    create endpoint for request headers, and grabbing data from request body

--- app/routes/index.py
+++ app/routes/index.py
@@ -1,18 +1,19 @@
 from app import flaskPgApp
-import json
-from flask import request
+from flask import request, json
 
 @flaskPgApp.route("/")
 def index():
+  statusCode = int(200)
   context = {
     "message" : "Hey",
     "success" : True,
-    "data" : []
+    "data" : [],
   }
-  return json.dumps(context)
+  return json.dumps(context), statusCode
 
 @flaskPgApp.route("/hello/<string:name>")
 def sayHello(name):
+  statusCode = int(200)
   context = {
     "message" : "route params",
     "data" : [
@@ -22,10 +23,11 @@ def sayHello(name):
     ],
     "success" : True
   }
-  return json.dumps(context)
+  return json.dumps(context), statusCode
 
 @flaskPgApp.route("/dummy/search")
 def dummySearch():
+  statusCode = int(200)
   context = {
     "message" : "Req Args, Query Params",
     "success" : True,
@@ -35,5 +37,46 @@ def dummySearch():
       }
     ]
   }
-  return json.dumps(context)
+  return json.dumps(context), statusCode
+
+@flaskPgApp.route("/dummy/post/headers")
+def dummyHeaders():
+  statusCode = int(200)
+  context = {
+    "success" : True,
+    "message" : "Request Headers Values"
+  }
+  
+  if request.headers['Content-Type'] == 'text/plain':
+    context["data"] = [
+      {
+        "text-data" : str(request.data)
+      }
+    ]
+    return json.dumps(context), statusCode
+  
+  if request.headers['Content-Type'] == 'application/json':
+    context["data"] = [
+      {
+        "json-data" : request.json
+      }
+    ]
+    return json.dumps(context), statusCode
+  
+  if request.headers['Content-Type'] == 'application/x-www-form-urlencoded':
+    context["data"] = [
+      {
+        "form-data-body" : {
+          "num1" : request.form["num1"],
+          "num3" : request.form["num3"]
+        }
+      }
+    ]
+    return json.dumps(context), statusCode
+  else:
+    context["data"] = []
+    context["success"] = False
+    context["message"] = "Content Type not processable"
+    return json.dumps(context), statusCode
+
 
